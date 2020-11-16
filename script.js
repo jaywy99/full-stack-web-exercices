183b764656f83d26f5dc658799146e210e7d5885
michelk-byte <michel.khoury.w@hotmail.com>
2020-11-16 11:54:10 +0200

    Assignement 4

---
+++ script.js
@@ -0,0 +1,22 @@
+
+$("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+$("#reserveform").click(function(){
+    $("#reservemodal").modal("show");
+});
+  
+$("#loginaccount").click(function(){
+    $("#loginModal").modal("show");
+});
+  
